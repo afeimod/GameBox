57ec39df40e9d9544e885cffa9d8132b401392bb
afei <165532911+afeimod@users.noreply.github.com>
2026-08-10 18:13:07 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 2
-        versionName = "2.1.0"
+        versionName = "2.2.0"
         ndkVersion = (project.findProperty("ndkVersion") as String?) ?: "26.3.11579264"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
